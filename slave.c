2ff3bdf6bf5f4fe22c2c8eda5b07023f17a6971a
pcurti <pcurti@itba.edu.ar>
2023-04-07 02:33:58 -0300

    to compile: gcc -Wall -o slave.out slave.c -lcrypto -lssl ; missing full err check and proper comments

--- slave.c
+++ slave.c
@@ -4,3 +4,58 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <openssl/md5.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+pthread_mutex_t mutex;
+
+#define BUFFER_SIZE 1024
+
+
+void write_shared_mem(unsigned char * md5) {
+  pthread_mutex_lock(&mutex);
+
+  for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        printf("%02x",md5[i]);
+    }
+  pthread_mutex_unlock(&mutex);
+  return;
+}
+
+void * md5_calculate(void * filedir) {
+  FILE * file = fopen(filedir, "r");
+  if (file == NULL)
+    return NULL;
+
+  MD5_CTX ctx;
+  MD5_Init(&ctx);
+  unsigned char * md5 = (unsigned char *) malloc(sizeof(unsigned char) * MD5_DIGEST_LENGTH);
+  if(md5 == NULL)
+      return NULL;
+  char buffer[BUFFER_SIZE];
+  size_t bytes_read;
+
+  while((bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, file)) != 0) {
+    MD5_Update(&ctx, buffer, bytes_read);
+  }
+  
+  MD5_Final(md5, &ctx);
+  fclose(file);
+  write_shared_mem(md5);
+  printf(" %s\n", (char *)filedir);
+  return NULL;
+}
+
+int main(int argc, char * argv[]) {
+  if(argc < 3)
+    return 1;
+  pthread_t thread1, thread2;
+  pthread_create(&thread1, NULL, md5_calculate, (void *) argv[0]);
+  pthread_create(&thread2, NULL, md5_calculate,  (void *) argv[1]);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+
+  return 0;
+}
