0e2d0e1fb93d10cc6e0bbcf70a17ef1562061bea
pcurti <pcurti@itba.edu.ar>
2023-04-09 14:41:37 -0300

    master and slave implementation, needs debugging

--- master.c
+++ master.c
@@ -5,24 +5,123 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/select.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 #define SLAVE_MAX 5
+#define BLOCK 5
 
-//recibe los archivos para encriptar
-//distribuye entre los esclavos y les da mas archivos si es que faltan ni bien terminan
-//recibe el resultado de los esclavos y lo carga en un buffer por orden de llegada
-//le entrega el buffer al proceso vista
-//guarda todo el output en un archivo resultado
+
+int pipes_array[SLAVE_MAX][2];
+
+
+char * int_to_str(int src);
 
 int main(int argc, char *argv[]){
-    if(argc == 1){
-        printf("Cantidad incorrecta de argumentos\n");
-        return 0;
-    }
+  errno = 0;
+  if(argc == 1){
+    printf("Cantidad incorrecta de argumentos\n");
+    return 0;
+  }
 
-    int argCant = argc -1;
-    for(int i = 1; i < argCant; i++){
-        DIR * directory = argv[i];
-
+  //crear slaves, y abrir los pipes
+  size_t slaves = (argc) / 2 > SLAVE_MAX? SLAVE_MAX:(argc)/2;
+  printf("slaves created: %d\n", slaves);
+  for(size_t i = 0; i < slaves; i++) {
+    if(pipe(pipes_array[i]) != 0) {
+      perror("couldn't create pipe, exiting");
+      return errno;
     }
-}   
+    printf("creating pipe nro: %d\n", i+1);
+    if(fork() != 0) {
+      //parent process
+      close(pipes_array[i][0]);
+    } else {
+
+      //child process
+      printf("child nro: %d\n", i+1);
+      close(pipes_array[i][1]);
+      char* pipefd = int_to_str(pipes_array[i][0]);
+      if(pipefd == NULL) {
+        return errno;
+      }
+
+      printf("pipes fd made str, now will execve to slave\n");
+      execve("slave.out", &pipefd, NULL);
+    }
+  }
+  
+
+  size_t array_index = 0;
+  fd_set rfds[slaves];
+  for(;array_index < slaves; array_index++) {
+    FD_ZERO(&rfds[array_index]);
+    FD_SET(pipes_array[array_index][0], &rfds[array_index]);
+  }
+  printf("setted FD sets, now commencig to send files dirs\n");
+  array_index = 0;
+  int result = 0;
+  unsigned short writes = 0;
+  size_t argv_index = 0;
+  while(argv_index < argc) {
+    int ready = 0;
+    while (!ready) {
+      FD_ZERO(&rfds[array_index]);
+      FD_SET(pipes_array[array_index][0], &rfds[array_index]);
+      result = select(pipes_array[array_index][0] + 1, &rfds[array_index],NULL, NULL, NULL);
+      if (result < 0) {
+        perror("select failed");
+        return 1;
+      }
+      else if (result == 0) {
+        continue;
+      }
+      else if (FD_ISSET(pipes_array[array_index][0], &rfds[array_index])) {
+        ready = 1;
+      }
+    }
+    write(pipes_array[array_index][1], argv[argv_index], strlen(argv[argv_index]));
+    if(writes == 2) {
+      writes++;
+      array_index++;
+    }
+    array_index = (array_index + 1) % slaves;
+  }
+  array_index = 0;
+  for(;array_index < slaves;array_index++)
+    close(pipes_array[array_index][1]);
+
+  array_index = 0;
+  for(;array_index < slaves; array_index++)
+    wait(NULL);
+  return 0;
+}
+
+
+
+
+char * int_to_str(int integer) {
+  char * pipefd = malloc(sizeof(char)*BLOCK);
+  int blocks = 1;
+  int copied_digits = 0;
+  if(pipefd == NULL) {
+    printf("failed while allocating mem to transfer fd value");
+    return NULL;
+  }
+  pipefd[copied_digits] = '0';
+  while(integer != 0) {
+    if(copied_digits == blocks * BLOCK) {
+      pipefd = realloc(pipefd, blocks * BLOCK + BLOCK);
+      if(pipefd == NULL) {
+        printf("failed while allocating mem to transfer fd value");
+        return NULL;
+      }
+    }
+    pipefd[copied_digits++] = integer % 10 + '0';
+    integer/=10;
+  }
+  pipefd = realloc(pipefd, copied_digits + 1);
+  pipefd[copied_digits] = '\0';
+  return pipefd;
+}
--- slave.c
+++ slave.c
@@ -7,10 +7,11 @@
 #include <openssl/md5.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 pthread_mutex_t mutex;
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 256
 
 
 void write_shared_mem(unsigned char * md5) {
@@ -47,15 +48,41 @@ void * md5_calculate(void * filedir) {
   return NULL;
 }
 
+
+int str_to_int(char * str) {
+  int number = 0;
+  while(*str) {
+    number += *str - '0';
+    number += 10;
+    *(str++);
+  }
+
+  return number;
+}
+
+
 int main(int argc, char * argv[]) {
-  if(argc < 3)
+  if(argc != 2)
     return 1;
+  int pipefd = str_to_int(argv[0]);
+
   pthread_t thread1, thread2;
-  pthread_create(&thread1, NULL, md5_calculate, (void *) argv[0]);
-  pthread_create(&thread2, NULL, md5_calculate,  (void *) argv[1]);
-
-  pthread_join(thread1, NULL);
-  pthread_join(thread2, NULL);
-
+  char buffer[BUFFER_SIZE];
+  size_t reads = 0;
+  size_t bytes_read;
+  while((bytes_read = read(pipefd, buffer, BUFFER_SIZE)) != 0) {
+    buffer[bytes_read + 1] = '\0';
+    if(reads == 0) {
+      pthread_create(&thread1, NULL, md5_calculate, (void *)buffer);
+      pthread_join(thread1, NULL);
+      reads++;
+    } else if (reads == 1) {
+      pthread_create(&thread2, NULL, md5_calculate,  (void *) argv[1]);
+      pthread_join(thread2, NULL);
+      reads++;
+    } else {
+      reads = 0;
+    }
+  }
   return 0;
 }
