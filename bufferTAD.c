1b4146bdda4ec59866a73d6643090736965b7ac6
Felipe Hiba <fhiba@itba.edu.ar>
2023-04-17 11:47:34 -0300

    Completed TAD refactor (for now)
    added reading functions and corrected an error in ftruncate

--- bufferTAD.c
+++ bufferTAD.c
@@ -19,8 +19,9 @@ typedef struct buffTAD{
     mode_t mode;
 }buffTAD;
 
-smh_data start_shm(mode_t new_mode){
-    smh_data new_shm = calloc(1,sizeof(buffTAD));
+//initialize the shared mem
+shm_data start_shm(mode_t new_mode){
+    shm_data new_shm = calloc(1,sizeof(buffTAD));
     if(new_shm == NULL){
         perror("There was an error while allocating space for the shared memory\n");
     }
@@ -32,16 +33,17 @@ smh_data start_shm(mode_t new_mode){
 }
 
 //wrapper for shm_open
-void buffer_open(smh_data info){
+void buffer_open(shm_data info){
     info->fd = shm_open("buffer",O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if(info->fd == -1){
         perror("There was an error while opening the shared memory\n");
     }
-    if(ftruncate(info->fd,512) == SEM_FAILED)
+    if(ftruncate(info->fd,512) != 0)
         perror("There was an error while creating space for the shared memory\n");
 }
 
-void buffer_up(smh_data info ){ 
+//wrapper for sem_post
+void buffer_up(shm_data info ){ 
     int aux = sem_post(info->sem_id);
     if(aux == -1){
         perror("There was an error while leaving the semaphore\n");
@@ -49,7 +51,8 @@ void buffer_up(smh_data info ){
     }
 }
 
-void buffer_down(smh_data info){ // wrapper for sem_wait
+//wrapper for sem_wait
+void buffer_down(shm_data info){ // wrapper for sem_wait
     int aux = sem_wait(info->sem_id);
     if(aux == -1){
         perror("There was an error while accesing the semaphore\n");
@@ -58,7 +61,7 @@ void buffer_down(smh_data info){ // wrapper for sem_wait
 }
 
 //wrapper for mmap
-void buffer_map(smh_data info){
+void buffer_map(shm_data info){
     void * pos = mmap(NULL, 512,info->mode,MAP_SHARED,info->fd,0);
     if(pos == MAP_FAILED){
         perror("There was an error while mapping the shared memory\n");
@@ -67,7 +70,8 @@ void buffer_map(smh_data info){
     info->last_seen = pos;
 }
 
-void buffer_close(smh_data info){
+//closes the semaphore, unmaps the shared mem, and closes the shared mem
+void buffer_close(shm_data info){
     if(sem_close(info->sem_id) == -1)
         perror("There was an error while closing the semaphore\n");
     if(munmap(info->first_pos,512) == -1)
@@ -76,7 +80,8 @@ void buffer_close(smh_data info){
         perror("There was an error while closing the shared memory\n");
 }
 
-void load_max_files(smh_data info, int max){
+//write in the first pos of the shared mem the total amount of files that will be processed
+void load_max_files(shm_data info, int max){
     int * first = info->first_pos;
     *first = max;
     info->last_seen+=sizeof(int);
@@ -84,7 +89,8 @@ void load_max_files(smh_data info, int max){
         perror("There was a problem while accessing the semaphore\n");
 }
 
-void load_buff(smh_data info, char * data){
+//load the shared mem with the result data
+void load_buff(shm_data info, char * data){
     if(info->first_pos == NULL)
         perror("Shared memory not initialized\n");
     int size = strlen(data);
@@ -95,4 +101,21 @@ void load_buff(smh_data info, char * data){
     buffer_up(info);
 }
 
+//returns the amount of files that where given to process to the master
+int read_max_files(shm_data info){
+    buffer_down(info);
+    int * pos = (int *)info->last_seen;
+    int out = *pos;
+    info->last_seen+= sizeof(int);
+    return out;
+}
 
+//loads in out what is inside the buffer
+void read_buff(shm_data info, char * out){
+    buffer_down(info);
+    size_t length = *((int*)info->last_seen);
+    info->last_seen += sizeof(int);
+    if(snprintf(out,length,"%s",(char *)info->last_seen) < 0)
+        perror("There was an error while reading from the shared memory\n");
+    info->last_seen += length;
+}
--- bufferTAD.h
+++ bufferTAD.h
@@ -8,19 +8,21 @@
 #include <semaphore.h>
 #include <stdlib.h>
 
-typedef struct buffTAD * smh_data;
+typedef struct buffTAD * shm_data;
 
 void create_sem(char * name);
-void load_buff(smh_data info, char * data);
-char * read_buff();
-void buffer_open(smh_data info);
-void buffer_map(smh_data info);
+void load_max_files(shm_data info, int max);
+void load_buff(shm_data info, char * data);
+int read_max_files(shm_data info);
+void read_buff(shm_data info, char * out);
+void buffer_open(shm_data info);
+void buffer_map(shm_data info);
 int buffer_unmap(void *addr, size_t length);
 void buffer_sem_open();
-void buffer_up(smh_data info);
-void buffer_down(smh_data info);
-void buffer_close(smh_data info);
-smh_data start_shm(mode_t new_mode);
-void load_max_files(smh_data info, int max);
+void buffer_up(shm_data info);
+void buffer_down(shm_data info);
+void buffer_close(shm_data info);
+shm_data start_shm(mode_t new_mode);
+
 
 #endif
